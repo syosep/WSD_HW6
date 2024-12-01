d57ed2b70268d1da08fd4a483a325ca6b9a0ccf1
심요셉 <sim-yosep@sim-yoseb-ui-MacBookPro.local>
2024-12-01 11:29:11 +0900

    fix 404 error

--- src/main/java/com/example/springcrudproject/BoardController.java
+++ src/main/java/com/example/springcrudproject/BoardController.java
@@ -16,25 +16,25 @@ public class BoardController {
     @RequestMapping(value = "/board/list", method = RequestMethod.GET)
     public String boardlist(Model model) {
         model.addAttribute("list", boardDAO.getBoardList());
-        return "list";
+        return "board/list";
     }
 
     @RequestMapping(value = "/board/add", method = RequestMethod.GET)
     public String addPost() {
-        return "add";
+        return "board/add";
     }
 
     @RequestMapping(value = "/board/edit/{id}", method = RequestMethod.GET)
     public String editPost(@PathVariable("id") int id, Model model) {
         BoardVO boardVO = boardDAO.getBoard(id);
         model.addAttribute("boardVO", boardVO);
-        return "edit";
+        return "board/edit";
     }
 
     @RequestMapping(value = "/board/view/{id}", method = RequestMethod.GET)
     public String viewPost(@PathVariable("id") int id, Model model) {
         BoardVO boardVO = boardDAO.getBoard(id);
         model.addAttribute("boardVO", boardVO);
-        return "view";
+        return "board/view";
     }
 }
